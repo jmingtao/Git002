ee422d0c25ec926a579197de92c0909bcc05befc
JinMingTao <admin@JingMingTaos-Mac.local>
2014-05-26 17:47:08 +0900

    Initial Commit

---
+++ Github001/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Github001
+//
+//  Created by JinMingTao on 5/26/14.
+//  Copyright (c) 2014 jinmingtao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Github001/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Github001
+//
+//  Created by JinMingTao on 5/26/14.
+//  Copyright (c) 2014 jinmingtao. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
